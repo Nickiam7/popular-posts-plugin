0fafb4e327c1d94b85b5ea66d6cac58d3f397222
Nick McNeany <nmcneany@gmail.com>
2017-07-01 14:13:17 -0500

    add popular post widget

--- ppp.php
+++ ppp.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Popular Posts Plugin - PPP
 Plugin URI:  https://github.com/Nickiam7/popular-posts-plugin/
-Description: Popular posts plugin based on number of posts views.
+Description: Popular posts plugin based on number of post views.
 Version:     0.1.0
 Author:      Nick McNeany
 Author URI:  https://github.com/Nickiam7/
@@ -55,7 +55,114 @@ function ppp_display_views( $column_name ) {
 add_action( 'manage_posts_custom_column', 'ppp_display_views', 5, 2 );
 
 
+/**
+* Add Widget
+*/
+/**
+ * Adds PPP_Widget widget.
+ */
+class PPP_Widget extends WP_Widget {
 
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'ppp_widget', // Base ID
+			esc_html__( 'Popular Posts', 'ppp' ), // Name
+			array( 'description' => esc_html__( 'Show the most popular posts', 'ppp' ), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		} 
+
+		$args = array(
+			'post_type'           => 'post',
+			'posts_per_page'      => 5,
+			'meta_key'            => 'views',
+			'orderby'             => 'meta_value_num',
+			'order'               => 'DESC',
+			'ignore_sticky_posts' => true
+		);
+
+		//New Query
+		$the_query = new WP_Query( $args );
+		
+		//The Loop
+		if ( $the_query->have_posts() ) {
+			echo '<ul>';
+			while ( $the_query->have_posts() ) {
+				$the_query->the_post();
+				echo '<li>'; 
+				echo '<a href="' . get_the_permalink() . '" rel="bookmark"> ' ;
+				echo get_the_title();
+				echo ' (' . get_post_meta( get_the_ID(), 'views', true ) . ')';
+				echo '</a>';
+				echo '</li>';
+			}
+			echo '</ul>';
+			wp_reset_postdata();
+		} else {
+			echo "Sorry, no posts.";
+		}
+		wp_reset_postdata();
+
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'Popular Posts', 'ppp' );
+		?>
+		<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'ppp' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+} // class PPP_Widget
+
+// register PPP_Widget widget
+function register_ppp_widget() {
+    register_widget( 'PPP_Widget' );
+}
+add_action( 'widgets_init', 'register_ppp_widget' );
 
 
 
